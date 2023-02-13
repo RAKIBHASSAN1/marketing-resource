c44cd9c24f81ae8a47e8b9d19bf38e53f0fd04db
RAKIB HASSAN <47098282+RAKIBHASSAN1@users.noreply.github.com>
2023-02-13 19:03:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ menuToggle.addEventListener('click', () => {
 	menuToggle.classList.toggle('open');
 	nav.classList.toggle('open');
 });
+const searchBtn = document.getElementById('search-btn');
+const searchInput = document.getElementById('search');
+const galleryImages = document.querySelectorAll('.gallery img');
+
+searchBtn.addEventListener('click', () => {
+  const searchTerm = searchInput.value.toLowerCase();
+  galleryImages.forEach(image => {
+    const caption = image.getAttribute('alt').toLowerCase();
+    if (caption.includes(searchTerm)) {
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  });
+});
